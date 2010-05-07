6f90bd4a6d7869bf13a4992179d766e3ac101cc7
Jonas Baumann <j.baumann@4teamwork.ch>
2010-05-07 11:31:49 +0000

    udpated egg (history.txt, version, setup.py)

--- setup.py
+++ setup.py
@@ -1,32 +1,43 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2'
+version = open('ftw/publisher/core/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
+
+tests_require=[
+    'collective.testcaselayer',
+    ]
 
 setup(name='ftw.publisher.core',
       version=version,
-      description="Core package of Product publisher",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="Core package of Product publisher" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='ftw publisher core',
-      author='Jonas Baumann',
-      author_email='j.baumann@4teamwork.ch',
-      url='https://svn.4teamwork.ch/repos/ftw/ftw.publisher.core/',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.publisher.core/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'collective.testcaselayer',
-      ],
+        'setuptools',
+        ],
+      extras_require={
+        'tests': tests_require,
+        },
+      tests_require=tests_require,
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
