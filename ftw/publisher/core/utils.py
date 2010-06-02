a25c6a93c4058e386646b7641b4677bc42e41ce6
Jonas Baumann <j.baumann@4teamwork.ch>
2010-06-02 07:51:29 +0000

    added error logger support

--- ftw/publisher/core/utils.py
+++ ftw/publisher/core/utils.py
@@ -43,11 +43,14 @@ LOG_DATEFORMAT = '%Y-%m-%dT%H:%M:%S'
 @var LOG_FILENAME:      Filename for the log-file to log to.
 """
 LOG_FILENAME = 'publisher.log'
+ERROR_LOG_FILENAME = 'publisher.error.log'
 
 """
 @var logHandler:        FileHandler instance (see python logging module)
 """
 logHandler = None
+errorLogHandler = None
+
 
 def getPublisherLogger(name):
     """
@@ -62,7 +65,7 @@ def getPublisherLogger(name):
     global logHandler
     if not logHandler:
         # create new handler
-        filepath = getLogFilePath()
+        filepath = getLogFilePath(error_log=False)
         if filepath:
             logHandler = logging.FileHandler(filepath)
             # register formatter
@@ -73,17 +76,45 @@ def getPublisherLogger(name):
         logger.addHandler(logHandler)
     return logger
 
-def getLogFilePath():
+def getPublisherErrorLogger(name):
+    """
+    Creates a python logger which loggs into the custom publisher log.
+
+    @param name:    Name of the logger instance (see python logging module)
+    @type name:     string
+    @return:        Python logging object
+    """
+    # get logger
+    logger = logging.getLogger(name)
+    global errorLogHandler
+    if not errorLogHandler:
+        # create new handler
+        filepath = getLogFilePath(error_log=True)
+        if filepath:
+            errorLogHandler = logging.FileHandler(filepath)
+            # register formatter
+            errorLogHandler.setFormatter(logging.Formatter(fmt=LOG_FORMAT,
+                    datefmt=LOG_DATEFORMAT))
+    if errorLogHandler and errorLogHandler not in logger.handlers:
+        # register it
+        logger.addHandler(errorLogHandler)
+    return logger
+
+def getLogFilePath(error_log=False):
     """
     Returns the log file path for the publisher log file.
     Returns None if it cannot guess the default log file path.
     
     @return:        logfile path or None
     """
+    if error_log:
+        filename = ERROR_LOG_FILENAME
+    else:
+        filename = LOG_FILENAME
     # get default log file path
     for handler in logging.root.handlers:
         if isinstance(handler, loghandler.FileHandler):
             path = os.path.dirname(handler.baseFilename)
-            return os.path.join(path, LOG_FILENAME)
+            return os.path.join(path, filename)
     return None
 
