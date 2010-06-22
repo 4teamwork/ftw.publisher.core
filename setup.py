eef554ef25d0a3a471a128e2545313dce33f2da3
Jonas Baumann <j.baumann@4teamwork.ch>
2010-06-22 12:42:07 +0000

    Fixed fixed namespace_packages in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='ftw.publisher.core',
       url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.publisher.core/',
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['ftw'],
+      namespace_packages=['ftw', 'ftw.publisher'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
