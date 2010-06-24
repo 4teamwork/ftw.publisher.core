da494575f6f359cf9e7d986e26c292bfadb40d40
Jonas Baumann <j.baumann@4teamwork.ch>
2010-06-24 08:53:45 +0000

    Added string encoding / decoding methods which work with json

--- ftw/publisher/core/utils.py
+++ ftw/publisher/core/utils.py
@@ -51,6 +51,12 @@ ERROR_LOG_FILENAME = 'publisher.error.log'
 logHandler = None
 errorLogHandler = None
 
+EXPECTED_ENCODINGS = (
+    'utf8',
+    'iso-8859-1',
+    'latin1',
+    )
+
 
 def getPublisherLogger(name):
     """
@@ -70,7 +76,7 @@ def getPublisherLogger(name):
             logHandler = logging.FileHandler(filepath)
             # register formatter
             logHandler.setFormatter(logging.Formatter(fmt=LOG_FORMAT,
-                    datefmt=LOG_DATEFORMAT))
+                                                      datefmt=LOG_DATEFORMAT))
     if logHandler and logHandler not in logger.handlers:
         # register it
         logger.addHandler(logHandler)
@@ -94,7 +100,7 @@ def getPublisherErrorLogger(name):
             errorLogHandler = logging.FileHandler(filepath)
             # register formatter
             errorLogHandler.setFormatter(logging.Formatter(fmt=LOG_FORMAT,
-                    datefmt=LOG_DATEFORMAT))
+                                                           datefmt=LOG_DATEFORMAT))
     if errorLogHandler and errorLogHandler not in logger.handlers:
         # register it
         logger.addHandler(errorLogHandler)
@@ -104,7 +110,7 @@ def getLogFilePath(error_log=False):
     """
     Returns the log file path for the publisher log file.
     Returns None if it cannot guess the default log file path.
-    
+
     @return:        logfile path or None
     """
     if error_log:
@@ -117,4 +123,94 @@ def getLogFilePath(error_log=False):
             path = os.path.dirname(handler.baseFilename)
             return os.path.join(path, filename)
     return None
+
+
+def decode_for_json(value, additional_encodings=[]):
+    """ Json does not handle encodings, so we need to convert any strings in unicode in
+    a way which allows to convert it back on the receiver.
+    """
+    if additional_encodings:
+        encodings = list(EXPECTED_ENCODINGS) + list(additional_encodings)
+    else:
+        encodings = EXPECTED_ENCODINGS
+
+    # unicode
+    if isinstance(value, unicode):
+        return u'unicode:' + value
+
+    # encoded strings
+    elif isinstance(value, str):
+        for enc in encodings:
+            try:
+                return unicode(enc) + u':' + value.decode(enc)
+            except UnicodeDecodeError:
+                pass
+        raise
+
+    # lists, tuples, sets
+    elif type(value) in (list, tuple, set):
+        nval = []
+        for sval in value:
+            nval.append(decode_for_json(sval))
+        if isinstance(value, tuple):
+            return tuple(nval)
+        if isinstance(value, set):
+            return set(nval)
+        return nval
+
+    # dicts
+    elif isinstance(value, dict):
+        nval = {}
+        for key, sval in value.items():
+            key = decode_for_json(key)
+            sval = decode_for_json(sval)
+            nval[key] = sval
+        return nval
+
+    # others
+    else:
+        return value
+
+
+def encode_after_json(value):
+    """ Is the opposite of decode_for_json
+    """
+    # there should not be any encoded strings
+    if isinstance(value, str):
+        value = unicode(value)
+
+    # unicode
+    if isinstance(value, unicode):
+        encoding, nval = unicode(value).split(':', 1)
+        if encoding == u'unicode':
+            return nval
+        else:
+            return nval.encode(encoding)
+
+    # lists, tuples, sets
+    elif type(value) in (list, tuple, set):
+        nval = []
+        for sval in value:
+            nval.append(encode_after_json(sval))
+        if isinstance(value, tuple):
+            return tuple(nval)
+        elif isinstance(value, set):
+            return set(nval)
+        else:
+            return nval
+
+    # dicts
+    elif isinstance(value, dict):
+        nval = {}
+        for key, sval in value.items():
+            key = encode_after_json(key)
+            sval = encode_after_json(sval)
+            nval[key] = sval
+        return nval
+
+    # other types
+    else:
+        return value
+
+
 
