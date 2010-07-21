b3b0d1dcc0ecbef82bbf364e79c632be74a9be15
Jonas Baumann <j.baumann@4teamwork.ch>
2010-07-21 09:46:43 +0000

    setup.py: added missing dependencies

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='ftw.publisher.core',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+        'archetypes.schemaextender',
         'setuptools',
         ],
       extras_require={
