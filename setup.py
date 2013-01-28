f2adc67459e2d8143d1ea3fca909033a91d973c2
Jonas Baumann <jone@jone.ch>
2013-01-28 11:16:20 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -18,11 +18,14 @@ setup(name='ftw.publisher.core',
       long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
 
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='ftw publisher core',
