dd274d4838a5410b1d82476df41618bbe36b5d45
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-02-12 17:47:47 +0100

    Removed properties test from portlet test

--- ftw/publisher/core/tests/test_portlet_adapter.py
+++ ftw/publisher/core/tests/test_portlet_adapter.py
@@ -14,32 +14,6 @@ class TestPortletAdapter(TestCase):
 
     layer = PUBLISHER_EXAMPLE_CONTENT_INTEGRATION
 
-    def test_properties_adapter_setter(self):
-        adapter1 = getAdapter(self.layer['testdoc1'], IDataCollector,
-                              name="properties_data_adapter")
-        data = adapter1.getData()
-        # setter
-        adapter2 = getAdapter(self.layer['testdoc2'], IDataCollector,
-                              name="properties_data_adapter")
-        adapter2.setData(data, metadata=None)
-
-        # test boolean
-        self.assertEquals(getattr(self.layer['testdoc2'], 'bool'), True)
-        # test date
-        self.assertEquals(getattr(self.layer['testdoc2'], 'date'),
-                          DateTime('2000/01/01'))
-        # test float
-        self.assertEquals(getattr(self.layer['testdoc2'], 'float'),
-                          2.1000000000000001)
-        # test int
-        self.assertEquals(getattr(self.layer['testdoc2'], 'int'), 2)
-        # test lines
-        self.assertEquals(getattr(self.layer['testdoc2'], 'lines')[0], 'row1')
-        self.assertEquals(getattr(self.layer['testdoc2'], 'lines')[1], 'row2')
-        #test string
-        self.assertEquals(getattr(self.layer['testdoc2'], 'string'),
-                          'Hello World!')
-
     def test_portlets_adapter_getter(self):
         #getter
         adapter = getAdapter(self.layer['folder1'], IDataCollector,
