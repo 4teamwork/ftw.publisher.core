106f7db172c76e703fe8e3296a9c5d41fa05af8d
Jonas Baumann <jone@jone.ch>
2013-03-22 15:58:53 +0100

    Dexterity tests: use a schema for the custom FTI.

--- ftw/publisher/core/tests/interfaces.py
+++ ftw/publisher/core/tests/interfaces.py
@@ -2,6 +2,10 @@ from plone.directives import form
 from plone.namedfile.field import NamedBlobFile
 
 
+class IFoo(form.Schema):
+    pass
+
+
 class IFileSchema(form.Schema):
 
     form.primary('file')
