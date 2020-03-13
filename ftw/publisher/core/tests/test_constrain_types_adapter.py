558154758ad13090dc018f5a487429744a68553b
Daniel Jowett <d.jowett@4teamwork.ch>
2020-03-13 10:47:28 +0100

    Also test constrain_types_adapter when mode is DISABLED or ACQUIRE

--- ftw/publisher/core/tests/test_constrain_types_adapter.py
+++ ftw/publisher/core/tests/test_constrain_types_adapter.py
@@ -2,6 +2,8 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.publisher.core.interfaces import IDataCollector
 from ftw.publisher.core.testing import PUBLISHER_CORE_INTEGRATION_TESTING
+from Products.CMFPlone.interfaces.constrains import ACQUIRE
+from Products.CMFPlone.interfaces.constrains import DISABLED
 from Products.CMFPlone.interfaces.constrains import ENABLED
 from Products.CMFPlone.interfaces.constrains import IConstrainTypes
 from Products.CMFPlone.interfaces.constrains import ISelectableConstrainTypes
@@ -15,8 +17,19 @@ from zope.component import getAdapter
 class TestConstrainTypesAdapter(TestCase):
 
     layer = PUBLISHER_CORE_INTEGRATION_TESTING
+    standard_types = [
+        'Collection',
+        'Document',
+        'Folder',
+        'Link',
+        'File',
+        'Image',
+        'News Item',
+        'Event'
+    ]
 
     def test_data_getter(self):
+        self.maxDiff = None
         folder = create(Builder('folder').titled(u'The Folder'))
         constrain_types = ISelectableConstrainTypes(folder)
         constrain_types.setConstrainTypesMode(ENABLED)
@@ -31,12 +44,24 @@ class TestConstrainTypesAdapter(TestCase):
                          'immediately_addable': immediately_addable_types}
         self.assertEquals(expected_data, adapter.getData())
 
+        constrain_types.setConstrainTypesMode(DISABLED)
+        expected_data = {'mode': DISABLED, 'locally_allowed': self.standard_types,
+                         'immediately_addable': self.standard_types}
+        self.assertEquals(expected_data, adapter.getData())
+
+        constrain_types.setConstrainTypesMode(ACQUIRE)
+        expected_data = {'mode': ACQUIRE, 'locally_allowed': self.standard_types,
+                         'immediately_addable': self.standard_types}
+        self.assertEquals(expected_data, adapter.getData())
+
     def test_data_setter(self):
         folder = create(Builder('folder').titled(u'The Folder'))
 
         adapter = getAdapter(folder, IDataCollector, name='constrain_types_adapter')
         allowed_types = ['Collection', 'Document', 'Folder']
         immediately_addable_types = ['Collection', 'Folder']
+
+        # Test when mode = 'ENABLED'
         adapter.setData({'mode': ENABLED, 'locally_allowed': allowed_types,
                          'immediately_addable': immediately_addable_types},
                         metadata=None)
@@ -45,3 +70,25 @@ class TestConstrainTypesAdapter(TestCase):
         self.assertEquals(ENABLED, constrain_types.getConstrainTypesMode())
         self.assertEquals(allowed_types, constrain_types.getLocallyAllowedTypes())
         self.assertEquals(immediately_addable_types, constrain_types.getImmediatelyAddableTypes())
+
+        # Test when mode = 'DISABLED'
+        # Results of getLocallyAllowedTypes() and getImmediatelyAddableTypes()
+        # are determined by the portal type
+        adapter.setData({'mode': DISABLED, 'locally_allowed': allowed_types,
+                         'immediately_addable': immediately_addable_types},
+                        metadata=None)
+        constrain_types = IConstrainTypes(folder)
+        self.assertEquals(DISABLED, constrain_types.getConstrainTypesMode())
+        self.assertEquals(self.standard_types, constrain_types.getLocallyAllowedTypes())
+        self.assertEquals(self.standard_types, constrain_types.getImmediatelyAddableTypes())
+
+        # Test when mode = 'ACQUIRE'
+        # Results of getLocallyAllowedTypes() and getImmediatelyAddableTypes()
+        # are determined by the parent object
+        adapter.setData({'mode': ACQUIRE, 'locally_allowed': allowed_types,
+                         'immediately_addable': immediately_addable_types},
+                        metadata=None)
+        constrain_types = IConstrainTypes(folder)
+        self.assertEquals(ACQUIRE, constrain_types.getConstrainTypesMode())
+        self.assertEquals(self.standard_types, constrain_types.getLocallyAllowedTypes())
+        self.assertEquals(self.standard_types, constrain_types.getImmediatelyAddableTypes())
