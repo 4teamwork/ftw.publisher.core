c6f22f0338cc4314b0ca7f618ccf49b43cbfcd02
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2021-04-01 09:58:46 -0400

    Fix collective.z3cform.datagridfield import.

--- ftw/publisher/core/tests/behaviors.py
+++ ftw/publisher/core/tests/behaviors.py
@@ -1,5 +1,5 @@
-from collective.z3cform.datagridfield import DataGridFieldFactory
-from collective.z3cform.datagridfield import DictRow
+from collective.z3cform.datagridfield.datagridfield import DataGridFieldFactory
+from collective.z3cform.datagridfield.row import DictRow
 from ftw.referencewidget.sources import ReferenceObjSourceBinder
 from ftw.referencewidget.widget import ReferenceWidgetFactory
 from plone.autoform import directives
